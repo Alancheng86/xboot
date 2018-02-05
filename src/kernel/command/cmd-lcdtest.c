c7451df48d52df6c528f6d77f32e7b4817062d64
kgo01633 <371403578@qq.com>
2018-02-05 21:07:22 +0800

    lcdtest加入system指令，调用按键进行手自动切换画面，使用IO进行驱动LED测试正常。进行下一步，尝试动态修改fb的参数并重新输出信号

--- src/kernel/command/cmd-lcdtest.c
+++ src/kernel/command/cmd-lcdtest.c
@@ -8,6 +8,8 @@
 #include <input/input.h>
 #include <input/keyboard.h>
 #include <command/command.h>
+#include <gpio/gpio.h>
+
 
 static void usage(void)
 {
@@ -112,6 +114,368 @@ static void cairo_show_title(cairo_t * cr, double width, double height, cairo_fo
 	cairo_restore(cr);
 }
 
+static int lcd_color(int argc, char ** argv)
+{
+	struct event_t e;
+	struct display_t * display = display_alloc(NULL);
+	cairo_font_face_t * face = cairo_font_face_alloc("/framework/assets/fonts/Roboto-Regular.ttf");
+	cairo_t * cr = display_present(display);
+	int width = display_width(display);
+	int height = display_height(display) - 32;
+	//int cwidth = cairo_image_surface_get_width(blue_bar);
+	//int cheight = cairo_image_surface_get_height(blue_bar);
+	double r, g, b;
+	double c = 1;
+	double t;
+
+	while(1)
+	{
+		
+		if(pump_event(runtime_get()->__event_base, &e))
+		{
+			if(e.type == EVENT_TYPE_KEY_UP)
+			{
+				if(e.e.key_up.key == KEY_EXIT)
+					{printf("    key_exit   \r\n");
+					break;}
+				if(e.e.key_up.key == KEY_ENTER)
+					{printf("    KEY_ENTER   \r\n");
+					break;}
+				if(e.e.key_up.key == KEY_VOLUME_UP)
+					{
+						printf("    KEY_VOLUME_UP   \r\n");
+						c = c + 1.0;
+						printf("    c=%d  \r\n",c);
+					}
+				if(e.e.key_up.key == KEY_VOLUME_DOWN)
+					{
+						printf("    KEY_VOLUME_DOWN   \r\n");
+						c = c - 1.0;
+						printf("    c=%d  \r\n",c);
+					}
+			}
+		}
+		if(ctrlc())
+			break;
+
+		c = c + 1.0;
+		t = c / 10;
+		r = (sin(t * 0.5 + 0.3) + 1) / 2;
+		g = (sin(t * 0.8 + 0.2) + 1) / 2;
+		b = (sin(t * 1.3 + 0.6) + 1) / 2;
+
+		cairo_save(cr);
+		cairo_set_source_rgb(cr, 0.349019608, 0.352941176, 0.82745098);
+		cairo_paint(cr);
+		cairo_restore(cr);
+
+		cairo_save(cr);
+		cairo_paint(cr);
+		cairo_restore(cr);
+
+		cairo_save(cr);
+		cairo_set_line_width(cr, 0);
+		cairo_rectangle(cr, 0, 0, width, display_height(display));
+		cairo_set_source_rgb(cr, r, g, b);
+		cairo_fill_preserve(cr);
+		cairo_set_source_rgb(cr, 1, 1, 1);
+		cairo_stroke(cr);
+		cairo_restore(cr);
+
+		//cairo_show_title(cr, width, 32, face, "颜色测试");
+		//cairo_show_title(cr, width, 32, face, "lcd test");
+		cr = display_present(display);
+		mdelay(20);
+		
+	}
+
+	display_free(display);
+	return 0;
+}
+
+static int do_color(int argc, char ** argv)
+{
+	struct event_t e;
+	struct display_t * display = display_alloc(NULL);
+	cairo_font_face_t * face = cairo_font_face_alloc("/framework/assets/fonts/Roboto-Regular.ttf");
+	cairo_t * cr = display_present(display);
+	int width = display_width(display);
+	int height = display_height(display) - 32;
+	cairo_surface_t * blue_bar = cairo_image_surface_create_from_png("/framework/assets/images/color/blue-bar.png");
+	cairo_surface_t * blue_dot = cairo_image_surface_create_from_png("/framework/assets/images/color/blue-dot.png");
+	cairo_surface_t * green_bar = cairo_image_surface_create_from_png("/framework/assets/images/color/green-bar.png");
+	cairo_surface_t * green_dot = cairo_image_surface_create_from_png("/framework/assets/images/color/green-dot.png");
+	cairo_surface_t * red_bar = cairo_image_surface_create_from_png("/framework/assets/images/color/red-bar.png");
+	cairo_surface_t * red_dot = cairo_image_surface_create_from_png("/framework/assets/images/color/red-dot.png");
+	int cwidth = cairo_image_surface_get_width(blue_bar);
+	int cheight = cairo_image_surface_get_height(blue_bar);
+	double r, g, b;
+	double c = 1;
+	double t;
+	int lcdtest_num=0;
+	char AUTO_RUN=0;		/////0X00手动，
+
+	int GPIO_SET_TEST=0;
+	
+	gpio_set_cfg(GPIO_SET_TEST,0x01);
+	gpio_set_pull(GPIO_SET_TEST,GPIO_PULL_NONE);
+	gpio_set_drv(GPIO_SET_TEST,GPIO_DRV_WEAKER);
+	gpio_set_direction(GPIO_SET_TEST,GPIO_DIRECTION_OUTPUT);
+	gpio_set_rate(GPIO_SET_TEST,GPIO_RATE_FAST);
+
+	while(1)
+	{
+		
+		if(pump_event(runtime_get()->__event_base, &e))
+		{
+			if(e.type == EVENT_TYPE_KEY_UP)
+			{
+				if(e.e.key_up.key == KEY_EXIT)
+					{printf("    key_exit   \r\n");
+					break;}
+				if(e.e.key_up.key == KEY_ENTER)
+					{printf("    KEY_ENTER   \r\n");
+						AUTO_RUN= (~AUTO_RUN);}
+				if(e.e.key_up.key == KEY_VOLUME_UP)
+					{
+						printf("    KEY_VOLUME_UP   \r\n");
+						c = c + 1.0;
+						lcdtest_num++;
+						//gpio_set_value(GPIO_SET_TEST,1);
+						printf("    c=%d ,lcdtest_num=%d ; \r\n",c,lcdtest_num);
+						
+						system("write 1 /sys/device/led/led-gpio.0/brightness");
+						printf(" system_write_1 \r\n");
+					}
+				if(e.e.key_up.key == KEY_VOLUME_DOWN)
+					{
+						printf("    KEY_VOLUME_DOWN   \r\n");
+						c = c - 1.0;
+						lcdtest_num--;
+						//gpio_set_value(GPIO_SET_TEST,0);
+						printf("    c=%d ,lcdtest_num=%d ; \r\n",c,lcdtest_num);
+
+						system("write 0 /sys/device/led/led-gpio.0/brightness");
+						printf(" system_write_0 \r\n");
+					}
+			}
+		}
+		if(ctrlc())
+			break;
+		
+		c = c + 1.0;			
+					
+		switch(lcdtest_num)
+			{
+					case 0:		///////Red
+					r=1;g=0;b=0;
+		//			printf("0 \r\n");
+
+					cairo_save(cr);
+					cairo_set_line_width(cr, 0);
+					cairo_rectangle(cr, 0, 0, width, display_height(display));
+					cairo_set_source_rgb(cr, r, g, b);
+					cairo_fill_preserve(cr);
+					cairo_set_source_rgb(cr, 1, 1, 1);
+					cairo_stroke(cr);
+					cairo_restore(cr);
+					break;
+
+					case 1:		///////Green
+		//			printf("1 \r\n");
+
+					r=0;g=1;b=0;
+
+					cairo_save(cr);
+					cairo_set_line_width(cr, 0);
+					cairo_rectangle(cr, 0, 0, width, display_height(display));
+					cairo_set_source_rgb(cr, r, g, b);
+					cairo_fill_preserve(cr);
+					cairo_set_source_rgb(cr, 1, 1, 1);
+					cairo_stroke(cr);
+					cairo_restore(cr);
+					break;
+
+					case 2:		///////Blue
+		//			printf("2 \r\n");
+
+					r=0;g=0;b=1;
+
+					cairo_save(cr);
+					cairo_set_line_width(cr, 0);
+					cairo_rectangle(cr, 0, 0, width, display_height(display));
+					cairo_set_source_rgb(cr, r, g, b);
+					cairo_fill_preserve(cr);
+					cairo_set_source_rgb(cr, 1, 1, 1);
+					cairo_stroke(cr);
+					cairo_restore(cr);
+					break;
+
+					case 3:		///////white
+			//		printf("3 \r\n");
+
+					r=1;g=1;b=1;
+
+					cairo_save(cr);
+					cairo_set_line_width(cr, 0);
+					cairo_rectangle(cr, 0, 0, width, display_height(display));
+					cairo_set_source_rgb(cr, r, g, b);
+					cairo_fill_preserve(cr);
+					cairo_set_source_rgb(cr, 1, 1, 1);
+					cairo_stroke(cr);
+					cairo_restore(cr);
+					break;
+
+					case 4:		///////grey
+			//		printf("4 \r\n");
+
+					r=0.5;g=0.5;b=0.5;
+
+					cairo_save(cr);
+					cairo_set_line_width(cr, 0);
+					cairo_rectangle(cr, 0, 0, width, display_height(display));
+					cairo_set_source_rgb(cr, r, g, b);
+					cairo_fill_preserve(cr);
+					cairo_set_source_rgb(cr, 1, 1, 1);
+					cairo_stroke(cr);
+					cairo_restore(cr);
+					break;
+
+					case 5:		///////black
+			//		printf("5 \r\n");
+
+					r=0;g=0;b=0;
+
+					cairo_save(cr);
+					cairo_set_line_width(cr, 0);
+					cairo_rectangle(cr, 0, 0, width, display_height(display));
+					cairo_set_source_rgb(cr, r, g, b);
+					cairo_fill_preserve(cr);
+					cairo_set_source_rgb(cr, 1, 1, 1);
+					cairo_stroke(cr);
+					cairo_restore(cr);
+					break;
+
+					case 6:		//////
+					//printf("6 \r\n");
+
+					r=1;g=1;b=0;
+					
+					cairo_save(cr);
+					cairo_set_line_width(cr, 0);
+					cairo_rectangle(cr, 0, 0, width, display_height(display));
+					cairo_set_source_rgb(cr, r, g, b);
+					cairo_fill_preserve(cr);
+					cairo_set_source_rgb(cr, 1, 1, 1);
+					cairo_stroke(cr);
+					cairo_restore(cr);
+					break;
+
+					case 7:		///////
+					//printf("6 \r\n");
+
+					r=1;g=0;b=1;
+					
+					cairo_save(cr);
+					cairo_set_line_width(cr, 0);
+					cairo_rectangle(cr, 0, 0, width, display_height(display));
+					cairo_set_source_rgb(cr, r, g, b);
+					cairo_fill_preserve(cr);
+					cairo_set_source_rgb(cr, 1, 1, 1);
+					cairo_stroke(cr);
+					cairo_restore(cr);
+					break;
+
+					case 8:		///////
+					//printf("6 \r\n");
+
+					r=0;g=1;b=1;
+					
+					cairo_save(cr);
+					cairo_set_line_width(cr, 0);
+					cairo_rectangle(cr, 0, 0, width, display_height(display));
+					cairo_set_source_rgb(cr, r, g, b);
+					cairo_fill_preserve(cr);
+					cairo_set_source_rgb(cr, 1, 1, 1);
+					cairo_stroke(cr);
+					cairo_restore(cr);
+					break;
+
+				default:
+			//		printf("default_info \r\n");
+					t = c / 10;
+					r = (sin(t * 0.5 + 0.3) + 1) / 2;
+					g = (sin(t * 0.8 + 0.2) + 1) / 2;
+					b = (sin(t * 1.3 + 0.6) + 1) / 2;
+
+					/*cairo_save(cr);
+					cairo_set_source_rgb(cr, 0.349019608, 0.352941176, 0.82745098);
+					cairo_paint(cr);
+					cairo_restore(cr);*/
+
+					cairo_save(cr);
+					cairo_set_source_surface(cr, red_bar, 91, 230);
+					cairo_paint(cr);
+					cairo_set_source_surface(cr, green_bar, 91, 260);
+					cairo_paint(cr);
+					cairo_set_source_surface(cr, blue_bar, 91, 290);
+					cairo_paint(cr);
+					cairo_set_source_surface(cr, red_dot, 90 + r * 280, 222);
+					cairo_paint(cr);
+					cairo_set_source_surface(cr, green_dot, 90 + g * 280, 252);
+					cairo_paint(cr);
+					cairo_set_source_surface(cr, blue_dot, 90 + b * 280, 282);
+					cairo_paint(cr);
+					cairo_restore(cr);
+
+					cairo_save(cr);
+					cairo_set_line_width(cr, 0);
+					cairo_rectangle(cr, 0, 0, width, display_height(display));
+					cairo_paint(cr);
+					cairo_set_source_rgb(cr, r, g, b);
+					cairo_fill_preserve(cr);
+					cairo_set_source_rgb(cr, 1, 1, 1);
+					cairo_paint(cr);
+					cairo_stroke(cr);
+					cairo_restore(cr);
+
+					if(c>=10){c=c-10;}
+
+					//cairo_show_title(cr, width, 32, face, "颜色测试");
+					//cairo_show_title(cr, width, 32, face, "lcd test");
+					//cr = display_present(display);
+					//mdelay(20);
+					break;
+			}
+		cr = display_present(display);
+					mdelay(150);
+
+		if(AUTO_RUN!=0x00)
+		{
+			lcdtest_num++;mdelay(150);
+		}
+		
+		if(lcdtest_num>=10){lcdtest_num=lcdtest_num-10;}
+	//	printf("	c=%d ,lcdtest_num=%d ; \r\n",c,lcdtest_num);
+
+	//	mdelay(20);
+/*
+		c = c + 1.0;
+		
+		*/
+	}
+
+	cairo_surface_destroy(blue_bar);
+	cairo_surface_destroy(blue_dot);
+	cairo_surface_destroy(green_bar);
+	cairo_surface_destroy(green_dot);
+	cairo_surface_destroy(red_bar);
+	cairo_surface_destroy(red_dot);
+	display_free(display);
+	return 0;
+}
+
+/*
 static int do_color(int argc, char ** argv)
 {
 	struct event_t e;
@@ -132,9 +496,17 @@ static int do_color(int argc, char ** argv)
 	double c = 1;
 	double t;
 
+	int GPIO_SET_TEST=0;
+	
+	gpio_set_cfg(GPIO_SET_TEST,0x01);
+	gpio_set_pull(GPIO_SET_TEST,GPIO_PULL_NONE);
+	gpio_set_drv(GPIO_SET_TEST,GPIO_DRV_WEAKER);
+	gpio_set_direction(GPIO_SET_TEST,GPIO_DIRECTION_OUTPUT);
+	gpio_set_rate(GPIO_SET_TEST,GPIO_RATE_FAST);
+
 	while(1)
 	{
-		//printf("    key scan   \r\n");
+		
 		if(pump_event(runtime_get()->__event_base, &e))
 		{
 			if(e.type == EVENT_TYPE_KEY_UP)
@@ -142,15 +514,23 @@ static int do_color(int argc, char ** argv)
 				if(e.e.key_up.key == KEY_EXIT)
 					{printf("    key_exit   \r\n");
 					break;}
-				/*if(e.e.key_up.key == KEY_ENTER)
-					{printf("    key_exit   \r\n");
+				if(e.e.key_up.key == KEY_ENTER)
+					{printf("    KEY_ENTER   \r\n");
 					break;}
 				if(e.e.key_up.key == KEY_VOLUME_UP)
-					{printf("    key_exit   \r\n");
-					break;}
+					{
+						printf("    KEY_VOLUME_UP   \r\n");
+						c = c + 1.0;
+						gpio_set_value(GPIO_SET_TEST,1);
+						printf("    c=%d  \r\n",c);
+					}
 				if(e.e.key_up.key == KEY_VOLUME_DOWN)
-					{printf("    key_exit   \r\n");
-					break;}*/
+					{
+						printf("    KEY_VOLUME_DOWN   \r\n");
+						c = c - 1.0;
+						gpio_set_value(GPIO_SET_TEST,0);
+						printf("    c=%d  \r\n",c);
+					}
 			}
 		}
 		if(ctrlc())
@@ -206,6 +586,7 @@ static int do_color(int argc, char ** argv)
 	display_free(display);
 	return 0;
 }
+*/
 
 static struct command_t cmd_color = {
 	.name	= "lcdtest",
